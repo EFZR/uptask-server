2816b134b111c0a2de2c5f1b78c2f908581afc0c
efzr <emerson.zapatarivas@gmail.com>
2024-06-09 00:36:13 -0600

    logging

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,8 +3,6 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whiteList = [process.env.FRONTEND_URL];
-    console.log(origin);
-    
 
     if (whiteList.includes(origin)) {
       callback(null, true);
